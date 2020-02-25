97efeb0f3e28c955026e6624ef83e45bf5f81ba1
NightKirie <qwer55113322@gmail.com>
2020-02-25 20:13:09 +0800

    Finish pre-sim in level3 image display control

--- Level_3/Image_Display_Control+Memory/lcd_ctrl.v
+++ Level_3/Image_Display_Control+Memory/lcd_ctrl.v
@@ -16,46 +16,117 @@ output reg done;    // 1 for controller finished writing to IRB
 
 reg [2:0] curr_x, curr_y;
 reg [3:0] curr_state, next_state;
+reg [5:0] IROM_A_pre, IRB_A_post;
 reg [7:0] buffer [0:63];
-//wire 		IROM_EN;
-parameter Reset = 3'b000;
-parameter Input = 3'b001;
-//assign	IROM_EN = (curr_state==Input) ? 1'b0 : 1'b1;
+reg [9:0] avg;
+
+parameter Input = 3'b000;
+parameter Command = 3'b001;
+parameter Output = 3'b010;
+
+parameter Write = 3'b000;
+parameter Shift_Up = 3'b001;
+parameter Shift_Down = 3'b010;
+parameter Shift_Left = 3'b011;
+parameter Shift_Right = 3'b100;
+parameter Average = 3'b101;
+parameter Mirror_X = 3'b110;
+parameter Mirror_Y = 3'b111;
+
 always@(posedge clk) begin
     if(reset) begin
-        
         busy <= 1;
+        IROM_A <= 0;
+        IROM_EN <= 0;
+        
         done <= 0;
         IRB_A <= 0;
         IRB_D <= 0;
         IRB_RW <= 1;
-        curr_state <= Reset;
+        
+        IROM_A_pre <= 0;
+        IRB_A_post <= 0;
+
+        curr_state <= Input;
     end
     else begin
         curr_state <= next_state;
         case(curr_state) 
             Input: begin
-               buffer[IROM_A] <= IROM_Q; 
+                curr_x <= 4;
+                curr_y <= 4;
+                buffer[IROM_A_pre] <= IROM_Q; 
+                IROM_A_pre <= IROM_A; 
+
+                IROM_A <= (IROM_A == 63) ? 0 : IROM_A + 1;
+                
+                IROM_EN <= (IROM_A_pre == 63) ? 1 : 0;
+                busy <= (IROM_A_pre == 63) ? 0 : 1;
+                
+            end
+            Command: begin
+                if(cmd_valid) begin
+                    case (cmd)
+                        Shift_Up: 
+                            curr_y <= (curr_y == 1) ? curr_y : curr_y - 1;
+                        Shift_Down:
+                            curr_y <= (curr_y == 7) ? curr_y : curr_y + 1;
+                        Shift_Left:
+                            curr_x <= (curr_x == 1) ? curr_x : curr_x - 1;
+                        Shift_Right:
+                            curr_x <= (curr_x == 7) ? curr_x : curr_x + 1;
+                        Average: begin
+                            buffer[(curr_y - 1) * 8 + curr_x - 1] <= avg;
+                            buffer[(curr_y - 1) * 8 + curr_x] <= avg;
+                            buffer[curr_y * 8 + curr_x - 1] <= avg;
+                            buffer[curr_y * 8 + curr_x] <= avg;
+                        end
+                        Mirror_X: begin
+                            buffer[(curr_y - 1) * 8 + curr_x - 1] <= buffer[curr_y * 8 + curr_x - 1];
+                            buffer[curr_y * 8 + curr_x - 1] <= buffer[(curr_y - 1) * 8 + curr_x - 1];
+                            buffer[(curr_y - 1) * 8 + curr_x] <= buffer[curr_y * 8 + curr_x];
+                            buffer[curr_y * 8 + curr_x] <= buffer[(curr_y - 1) * 8 + curr_x];
+                        end
+                        Mirror_Y: begin
+                            buffer[(curr_y - 1) * 8 + curr_x - 1] <= buffer[(curr_y - 1) * 8 + curr_x];
+                            buffer[(curr_y - 1) * 8 + curr_x] <= buffer[(curr_y - 1) * 8 + curr_x - 1];
+                            buffer[curr_y * 8 + curr_x - 1] <= buffer[curr_y * 8 + curr_x];
+                            buffer[curr_y * 8 + curr_x] <= buffer[curr_y * 8 + curr_x - 1];
+                        end
+                    endcase
+                end 
+            end
+            Output: begin
+                IRB_D <= buffer[IRB_A_post];
+                IRB_A <= IRB_A_post;
+                IRB_A_post <= (IRB_A_post == 63) ? 0 : IRB_A_post + 1;
+            
+                IRB_RW <= (IRB_A == 63) ? 1 : 0;
+                busy <= (IRB_A == 63) ? 0 : 1;
+                done <= (IRB_A == 63) ? 1 : 0;
             end
         endcase
     end
 end
 
 always@(*) begin
-    //next_state = curr_state;
     case(curr_state) 
-        Reset: begin
-            next_state = Input;
-            //IROM_EN <= 1;
-            IROM_A = 63;
-        end
         Input: begin
-            IROM_A = (IROM_A == 63) ? 0 : IROM_A + 1;
-            IROM_EN = (IROM_A == 63) ? 1 : 0;
-            
-            next_state = Input;
-
+            next_state <= (IROM_A_pre == 63) ? Command : Input;
         end
+        Command: begin
+            case(cmd) 
+                Write: 
+                    next_state = Output;
+                Average:
+                    avg = ((buffer[(curr_y - 1) * 8 + curr_x - 1] + buffer[(curr_y - 1) * 8 + curr_x]) + 
+                           (buffer[curr_y * 8 + curr_x - 1] + buffer[curr_y * 8 + curr_x])) >> 2;
+                default:
+                    avg = avg;
+            endcase
+        end
+
+
     endcase
     
 
--- Level_3/Image_Display_Control+Memory/work/@l@c@d_@c@t@r@l/_primary.vhd
+++ Level_3/Image_Display_Control+Memory/work/@l@c@d_@c@t@r@l/_primary.vhd
@@ -2,8 +2,17 @@ library verilog;
 use verilog.vl_types.all;
 entity LCD_CTRL is
     generic(
-        \Reset\         : vl_logic_vector(0 to 2) := (Hi0, Hi0, Hi0);
-        Input           : vl_logic_vector(0 to 2) := (Hi0, Hi0, Hi1)
+        Input           : vl_logic_vector(0 to 2) := (Hi0, Hi0, Hi0);
+        Command         : vl_logic_vector(0 to 2) := (Hi0, Hi0, Hi1);
+        Output          : vl_logic_vector(0 to 2) := (Hi0, Hi1, Hi0);
+        Write           : vl_logic_vector(0 to 2) := (Hi0, Hi0, Hi0);
+        Shift_Up        : vl_logic_vector(0 to 2) := (Hi0, Hi0, Hi1);
+        Shift_Down      : vl_logic_vector(0 to 2) := (Hi0, Hi1, Hi0);
+        Shift_Left      : vl_logic_vector(0 to 2) := (Hi0, Hi1, Hi1);
+        Shift_Right     : vl_logic_vector(0 to 2) := (Hi1, Hi0, Hi0);
+        Average         : vl_logic_vector(0 to 2) := (Hi1, Hi0, Hi1);
+        Mirror_X        : vl_logic_vector(0 to 2) := (Hi1, Hi1, Hi0);
+        Mirror_Y        : vl_logic_vector(0 to 2) := (Hi1, Hi1, Hi1)
     );
     port(
         clk             : in     vl_logic;
@@ -20,6 +29,15 @@ entity LCD_CTRL is
         done            : out    vl_logic
     );
     attribute mti_svvh_generic_type : integer;
-    attribute mti_svvh_generic_type of \Reset\ : constant is 1;
     attribute mti_svvh_generic_type of Input : constant is 1;
+    attribute mti_svvh_generic_type of Command : constant is 1;
+    attribute mti_svvh_generic_type of Output : constant is 1;
+    attribute mti_svvh_generic_type of Write : constant is 1;
+    attribute mti_svvh_generic_type of Shift_Up : constant is 1;
+    attribute mti_svvh_generic_type of Shift_Down : constant is 1;
+    attribute mti_svvh_generic_type of Shift_Left : constant is 1;
+    attribute mti_svvh_generic_type of Shift_Right : constant is 1;
+    attribute mti_svvh_generic_type of Average : constant is 1;
+    attribute mti_svvh_generic_type of Mirror_X : constant is 1;
+    attribute mti_svvh_generic_type of Mirror_Y : constant is 1;
 end LCD_CTRL;
