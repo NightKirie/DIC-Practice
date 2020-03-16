f8d104d09163133fad67fa5b81a38423d4f5c1a8
NightKirie <qwer55113322@gmail.com>
2020-03-16 21:01:21 +0800

    Spilt fft figure to a single pdf

--- Level_4/Fourier_Transform_Circuit/FAS/FAS.v
+++ Level_4/Fourier_Transform_Circuit/FAS/FAS.v
@@ -79,3 +79,41 @@ always @(*) begin
 end
 
 endmodule
+
+module bfly2_4(x,y,wr,wi,x0r,x0i,x1r,x1i);
+
+input signed [8:0]x,y;
+input signed [8:0]wr,wi;
+
+output[8:0]x0r,x0i,x1r,x1i;
+
+wire [17:0]p1,p2;
+
+assign p1=wr*y;
+assign p2=wi*y;
+assign x0r=x+p1[8:0];
+assign x0i=p2[8:0];
+assign x1r=x-p1[8:0];
+assign x1i=-p2[8:0];
+
+endmodule
+
+module bfly4_4(xr,xi,yr,yi,wr,wi,x0r,x0i,x1r,x1i);
+
+input signed [8:0]xr,xi,yr,yi;
+input signed [8:0]wr,wi;
+
+output [8:0]x0r,x0i,x1r,x1i;
+
+wire [17:0]p1,p2,p3,p4;
+
+assign p1=wr*yr;
+assign p2=wi*yi;
+assign p3=wr*yi;
+assign p4=wi*yr;
+assign x0r=xr+p1[17:8]-p2[17:8];
+assign x0i=xi+p3[17:8]+p4[17:8];
+assign x1r=xr-p1[17:8]+p2[17:8];
+assign x1i=xi-p3[17:8]-p4[17:8];
+
+endmodule
